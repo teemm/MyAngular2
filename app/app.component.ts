c446c01939755a28275bfc3b740b03cddb45b0cc
Temo Pkhaladze <kverna133@gmail.com>
2016-07-05 16:04:04 +0400

    ngFor tgif

---
+++ app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    templateUrl: 'app/app.component.html'
+})
+export class AppComponent {
+    persons = [
+        { name: 'gela', lastname: 'bendeliani', id:'1' },
+        { name: 'taso', lastname: 'gegenava', id:'2' },
+        { name: 'guja', lastname: 'mesxi', id:'3' },
+        { name: 'kekela', lastname: 'pantani', id:'4' },
+    ];
+}
